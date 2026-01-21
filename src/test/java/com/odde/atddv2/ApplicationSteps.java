9f414dd17468bed17a77877d7dbd964234c1cb6e
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2026-01-21 19:46:06 +0800

    :sparkles: clean up api tests and dependencies

--- src/test/java/com/odde/atddv2/ApplicationSteps.java
+++ src/test/java/com/odde/atddv2/ApplicationSteps.java
@@ -1,10 +1,12 @@
 package com.odde.atddv2;
 
 import com.github.leeonky.cucumber.restful.RestfulStep;
+import com.odde.atddv2.entity.User;
 import com.odde.atddv2.repo.OrderRepo;
 import com.odde.atddv2.repo.UserRepo;
 import io.cucumber.java.Before;
 import io.cucumber.spring.CucumberContextConfiguration;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.test.context.ContextConfiguration;
@@ -34,4 +36,16 @@ public class ApplicationSteps {
     public void setBaseUrl() {
         restfulStep.setBaseUrl("http://127.0.0.1:10082/api/");
     }
+
+    @SneakyThrows
+    @Before("@api-login")
+    public void apiLogin() {
+        User defaultUser = new User().setUserName("j").setPassword("j");
+        userRepo.save(defaultUser);
+        RestfulStep loginRestfulStep = new RestfulStep();
+        loginRestfulStep.setBaseUrl("http://localhost:10082");
+        loginRestfulStep.post("/users/login", defaultUser);
+        restfulStep.header("token", (String)loginRestfulStep.response("headers.Token"));
+    }
+
 }
